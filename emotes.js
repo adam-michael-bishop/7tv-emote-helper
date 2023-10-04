25a5b281562bff4bf723de278c85b564e02a0e68
Adam Bishop <adam.michael.bishop@gmail.com>
2023-10-04 17:22:26 -0500

    Add uploadAllEmotesFromSet Add isEmoteAdded

--- emotes.js
+++ emotes.js
@@ -1,22 +1,38 @@
-import "dotenv/config";
-import fetch from "node-fetch";
+import { getEmoteSet } from "./7tv-requests.js";
+import { encodeImageToBase64 } from "./utils.js";
+import {
+  VerifyDiscordRequest,
+  DiscordRequest,
+  createGuildEmoji,
+  getGuildEmojis,
+} from "./discord-requests.js";
 
-const host7TV = "https://7tv.io/v3";
+function isEmoteAdded(emoteToAdd, emoteList, applicationId) {
+  for (let i = 0; i < emoteList.length; i++) {
+    const checkedEmote = emoteList[i];
 
-export const getEmoteSet = async function (setId) {
-  const endpoint = "/emote-sets/";
-  const url = encodeURI(host7TV + endpoint + setId);
-  const res = await fetch(url, {
-    headers: {
-      "Content-Type": "application/json; charset=UTF-8",
-    },
-  });
-  // throw API errors
-  if (!res.ok) {
-    const data = await res.json();
-    console.log(res.status);
-    throw new Error(JSON.stringify(data));
+    if (checkedEmote.user.id !== applicationId) {
+      continue;
+    }
+    if (checkedEmote.name === emoteToAdd.name) {
+      return true;
+    }
   }
-  // return original response
-  return res.json();
-};
+
+  return false;
+}
+
+export async function uploadAllEmotesFromSet(setId, guildId, applicationId) {
+  const set = await getEmoteSet(setId);
+  const emoteList = await getGuildEmojis(guildId);
+
+  for (let i = 0; i < set.emotes.length; i++) {
+    const emote = set.emotes[i];
+    if (isEmoteAdded(emote, emoteList, applicationId)) continue;
+    const fileExtension = "/3x.webp";
+    const emoteURL = "https:" + emote.data.host.url + "/" + fileExtension;
+    const imageData = await encodeImageToBase64(emoteURL);
+
+    await createGuildEmoji(guildId, emote.name, imageData);
+  }
+}
