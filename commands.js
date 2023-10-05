06047a061b4a3245513a956f87928ab80292a6fc
Adam Bishop <adam.michael.bishop@gmail.com>
2023-10-05 15:59:17 -0500

    Add remove set command definition

--- commands.js
+++ commands.js
@@ -12,14 +12,30 @@ const ADD_EMOTE_SET_COMMAND = {
   name: "add-set",
   description: "Adds all emotes in 7tv emote set by ID to discord server",
   type: 1,
-  options: [{
-    name: "set-id",
-    description: "The ID of the set to add",
-    required: true,
-    type: 3,
-  }],
+  options: [
+    {
+      name: "set-id",
+      description: "The ID of the set to add",
+      required: true,
+      type: 3,
+    },
+  ],
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, ADD_EMOTE_SET_COMMAND];
+const REMOVE_EMOTE_SET_COMMAND = {
+  name: "remove-set",
+  description: "Removes all emotes in the 7tv set from the discord server",
+  type: 1,
+  options: [
+    {
+      name: "set-id",
+      description: "The ID of the set to remove",
+      required: true,
+      type: 3,
+    },
+  ],
+};
+
+const ALL_COMMANDS = [TEST_COMMAND, ADD_EMOTE_SET_COMMAND, REMOVE_EMOTE_SET_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
